d461d531c5990aa6b7b1532671606f5bf2656390
zEnder7072 <101995173+zEnder7072@users.noreply.github.com>
2024-05-16 23:25:25 -0300

    Update index.js

--- dio-desafio/index.js
+++ dio-desafio/index.js
@@ -1,1 +1,30 @@
-sadasd
+var nome = "Jogador"
+var exp = 1000
+var nivel
+
+if (exp<1001){
+    nivel = "Ferro"
+}
+else if (1000<exp<2001){
+    nivel = "Bronze"
+}
+else if (2000<exp<3001){
+    nivel = "Prata"
+}
+else if (3000<exp<4000){
+    nivel = "Ouro"
+}
+else if (4000<exp<5000){
+    nivel = "Platina"
+}
+else if (5000<exp<6000){
+    nivel = "Ascedente"
+}
+else if (6000<exp<7000){
+    nivel = "Imortal"
+}
+else{
+    nivel = "Radiante"
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
